211a1acbe2acdb1288dd2dea10c937837d9e8595
shubhayu-dev <shubhayusadhak@gmail.com>
2025-05-13 19:33:07 +0530

    deleted checks

--- check.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-# Access the API key stored in secrets.toml
-api_key = st.secrets["openrouter"]["API_KEY"]
-
-# You can now use the api_key securely
-print(api_key)  # Test to ensure it's working correctly
